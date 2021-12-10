294f6ea73dd52d64619169985fd7ad1afe3c9f13
dzakiasarisuryaputra <91957941+dzakiasarisuryaputra@users.noreply.github.com>
2021-12-10 19:58:17 +0700

    Add files via upload

---
+++ minggu10/eksternal-js/kodeku.js
@@ -0,0 +1,5 @@
+alert("Hello, ini adalah program JS eksternal!");
+function myFunction(){
+    var nama = prompt ("Siapa Nama Kamu?"," ");
+    document.write ("<p>Hello "+nama+",saya adalah seorang mahasiswa Teknik Informatika dan saat ini saya sedang belajar JavaScript.</p>")
+    }
