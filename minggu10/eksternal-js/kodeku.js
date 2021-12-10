a85a9865d69e07e0792d12bdfa2c069ec44849f0
dzakiasarisuryaputra <91957941+dzakiasarisuryaputra@users.noreply.github.com>
2021-12-10 19:55:41 +0700

    Delete minggu10 directory

--- minggu10/eksternal-js/kodeku.js
+++
@@ -1,5 +0,0 @@
-alert("Hello, ini adalah program JS eksternal!");
-function myFunction(){
-    var nama = prompt ("Siapa Nama Kamu?"," ");
-    document.write ("<p>Hello "+nama+",saya adalah seorang mahasiswa Teknik Informatika dan saat ini saya sedang belajar JavaScript.</p>")
-    }
